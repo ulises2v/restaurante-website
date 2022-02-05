0731b2e4801ec21bb6d31fc952563c25829b0ef6
Christian Velázquez - U <chrisul2v@gmail.com>
2022-02-05 13:17:12 -0600

    terminado home

--- js/script.js
+++ js/script.js
@@ -28,3 +28,19 @@ window.onscroll = () => {
 closeBtn.onclick = () => {
     searchForm.classList.remove('active');
 }
+
+// swiper
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+  });
