1924173fb75fdb79cdc1e4de698e98dab99b50e8
Christian Velázquez - U <chrisul2v@gmail.com>
2022-02-05 11:36:39 -0600

    terminado search form

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
 // menu controls
 
-let menuBars = document.getElementById('menu-bars');
+let menuBtn = document.getElementById('menu-bars');
 let navB = document.querySelector('header .navbar');
 
-menuBars.onclick = () => {
-    menuBars.classList.toggle('fa-times');
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
     navB.classList.toggle('active');
+}
+
+// search controls
+
+let searchBtn = document.getElementById('search-icon');
+let closeBtn = document.getElementById('close');
+let searchForm = document.querySelector('#search-form');
+
+searchBtn.onclick = () => {
+    searchForm.classList.add('active');
+    navB.classList.remove('active');
+    menuBtn.classList.remove('fa-times');
+}
+
+window.onscroll = () => {
+    menuBtn.classList.remove('fa-times');
+    navB.classList.remove('active');
+}
+
+closeBtn.onclick = () => {
+    searchForm.classList.remove('active');
 }
