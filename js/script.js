223ca011cb649d59b132ed39119a5c14c5943d05
Christian Velázquez - U <chrisul2v@gmail.com>
2022-02-07 14:23:32 -0600

    terminado order, footer and scrolspy

--- js/script.js
+++ js/script.js
@@ -20,13 +20,37 @@ searchBtn.onclick = () => {
     menuBtn.classList.remove('fa-times');
 }
 
+closeBtn.onclick = () => {
+  searchForm.classList.remove('active');
+}
+
+// -----------------------------------------------------
+// ver tutorial scrollspy javascript en:
+// https://www.youtube.com/watch?v=fAAk9CATILc
+// --------------------------------------------------------
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
+
 window.onscroll = () => {
     menuBtn.classList.remove('fa-times');
     navB.classList.remove('active');
-}
 
-closeBtn.onclick = () => {
-    searchForm.classList.remove('active');
+    section.forEach(sec => {
+
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+      if(top >= offset && top < offset + height) {
+        navLinks.forEach(link => {
+          link.classList.remove('active');
+          document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+        });
+      }
+
+    });
 }
 
 // swiper
