4d1fd62ccdc3835f28c6ca67b9c53d271b829c09
Christian Velázquez - U <chrisul2v@gmail.com>
2022-02-08 21:02:10 -0600

    terminado loader

--- js/script.js
+++ js/script.js
@@ -92,3 +92,15 @@ var swiper = new Swiper(".home-slider", {
       },
     }
   });
+
+  // loader container
+
+  function loader() {
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+
+  function fadeOut() {
+    setInterval(loader, 3000);
+  }
+
+  window.onload = fadeOut();
